0e04a112825d4af66ab338431c97ed952d4fd8e9
ujjwal <ujjwal.12100544@lpu.in>
2024-07-18 19:04:26 +0530

    packages added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Divider } from "@chakra-ui/react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import home from "./pages/homepage";
+import chat from "./pages/chatpage";
 
 function App() {
-  return <div className="">hi</div>;
+  return (
+   <div>
+    <Routes>
+    <Route path="/" Component={home} />
+    <Route path="/chats" Component={chat}/>
+    </Routes>
+   </div>
+  );
 }
 
 export default App;
