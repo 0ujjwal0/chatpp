c7e05a45ea9e7e47119980efe7d8b0b75c7e12c7
ujjwal <ujjwal.12100544@lpu.in>
2024-07-18 19:40:00 +0530

    api testing

--- src/pages/chatpage.jsx
+++ src/pages/chatpage.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
 const Chatpage = () => {
+  const [chats,setchats]=useState([]);
+ const fetchchats=async()=>{
+    const {data}= await axios.get('/api/chat')
+   setchats(data);
+  }
+  useEffect(()=>{
+fetchchats();
+  },[])
+
   return (
-    <div>Chatpage</div>
+    <div>{chats.map((chat)=>(<div key={chat._id}>{chat.chatName}</div>))}</div>
   )
 }
 
