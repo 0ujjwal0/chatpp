606b5b5ae9617e13aff4b939ed4b0d3cf4db0d75
ujjwal <ujjwal.12100544@lpu.in>
2024-07-30 22:31:13 +0530

    signup functionality added

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,22 +1,83 @@
 import React, { useState } from "react";
-
+import { json } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const Signup = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmpass, setConfirmpass] = useState("");
   const [pic, setPic] = useState("");
-  const [error, setError] = useState("");
-
-  const handleSubmit = (e) => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (password !== confirmpass) {
-      setError("Passwords do not match");
+    setLoading(true);
+    if (!name || !email || !password || !confirmpass) {
+      toast.warning("Aww you missed some details, Please fill them out!!", {
+        autoClose: 3000,
+      });
+      setLoading(false);
       return;
     }
-    // Handle form submission
-    setError("");
-    // Proceed with form submission logic
+
+    if (password !== confirmpass) {
+      toast.error("Passwords do not match", { autoClose: 3000 });
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "/api/user",
+        { name, email, password, pic },
+        config
+      );
+      toast.success("Registration Successful", { autoClose: 3000 });
+      localStorage.setItem("userinfo", JSON.stringify(data));
+      setLoading(false);
+      navigate("/chats");
+    } catch (error) {
+      toast.error("error occured!!", { autoClose: 3000 });
+      setLoading(false);
+    }
+  };
+  const postDetails = (pics) => {
+    setLoading(true);
+    if (pics === undefined) {
+      toast.warning("opsiee, Please select an Image!", { autoClose: 3000 });
+      return;
+    }
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      toast.success("goodjob");
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "talkie");
+      data.append("cload_name", "ujjwal0");
+      fetch("https://api.cloudinary.com/v1_1/ujjwal0/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url.toString());
+          console.log(data.url.toString());
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          setLoading(false);
+        });
+    } else {
+      toast.warning("opsiee, Please select an Image!", { autoClose: 3000 });
+      setLoading(false);
+      return;
+    }
   };
 
   return (
@@ -77,52 +138,16 @@ const Signup = () => {
         >
           Confirm password
         </label>
-      </div>
-      {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
-      <div className="grid md:grid-cols-2 md:gap-6">
-        <div className="relative z-0 w-full mb-5 group">
-          <input
-            type="text"
-            name="floating_first_name"
-            id="floating_first_name"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-          <label
-            htmlFor="floating_first_name"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            First name
-          </label>
-        </div>
-        <div className="relative z-0 w-full mb-5 group">
-          <input
-            type="tel"
-            pattern="[0-9]{10}"
-            name="floating_phone"
-            id="floating_phone"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            required
-          />
-          <label
-            htmlFor="floating_phone"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Phone number (10 digits)
-          </label>
-        </div>
-        <div className="relative z-0 w-full mb-5 group">
+      
+       
+        <div className="relative z-0 w-full mb-5 group my-8">
           <input
             type="file"
             name="profile_picture"
             id="profile_picture"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
-            onChange={(e) => setPic(e.target.files[0])}
+            onChange={(e) => postDetails(e.target.files[0])}
             required
           />
           <label
@@ -137,8 +162,9 @@ const Signup = () => {
       <button
         type="submit"
         className="text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        disabled={loading}
       >
-        Sign Up
+        {loading ? "Loading..." : "Sign Up"}{" "}
       </button>
     </form>
   );
