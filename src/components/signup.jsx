559d02be80f4a825650b299d26f53702e20381a7
ujjwal <ujjwal.12100544@lpu.in>
2024-08-10 20:09:47 +0530

    sign in sign up changes

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,38 +1,38 @@
 import React, { useState } from "react";
-import { json } from "react-router-dom";
 import { toast } from "react-toastify";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+
 const Signup = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmpass, setConfirmpass] = useState("");
-  const [pic, setPic] = useState("");
+  const [pic, setPic] = useState(null);
+  const [picPreview, setPicPreview] = useState(null);
   const [loading, setLoading] = useState(false);
-  
+
   const navigate = useNavigate();
-  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    if (!name || !email || !password || !confirmpass) {
-      toast.warning("Aww you missed some details, Please fill them out!!", {
-        autoClose: 3000,
-      });
+    if (!name || !email || !password || !confirmpass || !pic) {
+      toast.warning("Please fill out all fields!", { autoClose: 3000 });
       setLoading(false);
       return;
     }
 
     if (password !== confirmpass) {
       toast.error("Passwords do not match", { autoClose: 3000 });
+      setLoading(false);
       return;
     }
 
     try {
       const config = {
         headers: {
-          "content-type": "application/json",
+          "Content-Type": "application/json",
         },
       };
       const { data } = await axios.post(
@@ -45,22 +45,23 @@ const Signup = () => {
       setLoading(false);
       navigate("/chats");
     } catch (error) {
-      toast.error("error occured!!", { autoClose: 3000 });
+      toast.error("An error occurred during registration", { autoClose: 3000 });
       setLoading(false);
     }
   };
+
   const postDetails = (pics) => {
     setLoading(true);
     if (pics === undefined) {
-      toast.warning("opsiee, Please select an Image!", { autoClose: 3000 });
+      toast.warning("Please select an image!", { autoClose: 3000 });
+      setLoading(false);
       return;
     }
     if (pics.type === "image/jpeg" || pics.type === "image/png") {
-      toast.success("goodjob");
       const data = new FormData();
       data.append("file", pics);
       data.append("upload_preset", "talkie");
-      data.append("cload_name", "ujjwal0");
+      data.append("cloud_name", "ujjwal0");
       fetch("https://api.cloudinary.com/v1_1/ujjwal0/image/upload", {
         method: "post",
         body: data,
@@ -68,17 +69,16 @@ const Signup = () => {
         .then((res) => res.json())
         .then((data) => {
           setPic(data.url.toString());
-          console.log(data.url.toString());
+          setPicPreview(URL.createObjectURL(pics));
           setLoading(false);
         })
         .catch((error) => {
-          console.log(error);
+          toast.error("Image upload failed", { autoClose: 3000 });
           setLoading(false);
         });
     } else {
-      toast.warning("opsiee, Please select an Image!", { autoClose: 3000 });
+      toast.warning("Please select a JPEG or PNG image!", { autoClose: 3000 });
       setLoading(false);
-      return;
     }
   };
 
@@ -87,6 +87,24 @@ const Signup = () => {
       className="max-w-md mx-auto flex flex-col"
       onSubmit={handleSubmit}
     >
+      <div className="relative z-0 w-full mb-5 group">
+        <input
+          type="text"
+          name="floating_name"
+          id="floating_name"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <label
+          htmlFor="floating_name"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Name
+        </label>
+      </div>
       <div className="relative z-0 w-full mb-5 group">
         <input
           type="email"
@@ -100,7 +118,7 @@ const Signup = () => {
         />
         <label
           htmlFor="floating_email"
-          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
           Email address
         </label>
@@ -118,7 +136,7 @@ const Signup = () => {
         />
         <label
           htmlFor="floating_password"
-          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
           Password
         </label>
@@ -136,29 +154,36 @@ const Signup = () => {
         />
         <label
           htmlFor="floating_repeat_password"
-          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
           Confirm password
         </label>
-      
-       
-        <div className="relative z-0 w-full mb-5 group my-8">
-          <input
-            type="file"
-            name="profile_picture"
-            id="profile_picture"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            onChange={(e) => postDetails(e.target.files[0])}
-            required
-          />
-          <label
-            htmlFor="profile_picture"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Profile Picture
-          </label>
-        </div>
+      </div>
+      <div className="relative z-0 w-full mb-5 group my-8">
+        <input
+          type="file"
+          name="profile_picture"
+          id="profile_picture"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          onChange={(e) => postDetails(e.target.files[0])}
+          required
+        />
+        <label
+          htmlFor="profile_picture"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Profile Picture
+        </label>
+        {picPreview && (
+          <div className="mt-4">
+            <img
+              src={picPreview}
+              alt="Profile Preview"
+              className="w-32 h-32 rounded-full object-cover mx-auto"
+            />
+          </div>
+        )}
       </div>
 
       <button
@@ -166,7 +191,7 @@ const Signup = () => {
         className="text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         disabled={loading}
       >
-        {loading ? "Loading..." : "Sign Up"}{" "}
+        {loading ? "Loading..." : "Sign Up"}
       </button>
     </form>
   );
